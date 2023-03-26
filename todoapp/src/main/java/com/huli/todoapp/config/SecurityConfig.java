0fd3c08e74448f5b8a02cea5b8c7ec0e0ceedf2c
Lubomir Drab <lubo.drab@gmail.com>
2023-03-26 12:37:58 +0200

    User entity and UserRepository added

--- todoapp/src/main/java/com/huli/todoapp/config/SecurityConfig.java
+++ todoapp/src/main/java/com/huli/todoapp/config/SecurityConfig.java
@@ -6,8 +6,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -23,18 +29,28 @@ public class SecurityConfig {
                 .and().httpBasic();
         return http.build();
     }
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailService(){
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("12345")
+//                .authorities("user")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin,user);
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailService(){
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .authorities("user")
-                .build();
-        return new InMemoryUserDetailsManager(admin,user);
+    public UserDetailsService userDetailsService (DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
